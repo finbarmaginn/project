378221f056a2f442e39a8deda08dafc924c0ed0b
finbarmaginn <finbarmaginn@gmail.com>
2017-11-30 21:14:15 +0000

    inserted jQuery and ready to start writing code in js/index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // write js to target and do different things to your html elements by using
+  // classes and different id's etc
+
+  // $(".phylink") will target all html elements in the page that have the class
+  // phylink
+
+  console.log("Little bit of code to get the ball rolling")
+});
