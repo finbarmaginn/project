523fb32bb3fcc000279ff3705eeec646b69459db
finbarmaginn <finbarmaginn@gmail.com>
2017-11-30 21:28:41 +0000

    solution

--- js/index.js
+++ js/index.js
@@ -5,5 +5,20 @@ $(document).ready(function() {
   // $(".phylink") will target all html elements in the page that have the class
   // phylink
 
-  console.log("Little bit of code to get the ball rolling")
+  // <img class="chemimg" src="https://cdn.pixabay.com/photo/2017/04/16/18/08/test-tube-2235388__340.png" />
+  // <img class="mathimg" src="https://cdn.pixabay.com/photo/2016/07/29/21/42/school-1555910__340.png" />
+
+  $(".phylink, .chemlink, .mathlink").hover(function() {
+    switch (this.className) {
+      case "phylink":
+        $(".switcher").attr("src", "https://cdn.pixabay.com/photo/2016/03/31/19/13/atom-1294810__340.png");
+        break;
+      case "chemlink":
+        $(".switcher").attr("src", "https://cdn.pixabay.com/photo/2017/04/16/18/08/test-tube-2235388__340.png");
+        break;
+      case "mathlink":
+        $(".switcher").attr("src", "https://cdn.pixabay.com/photo/2016/07/29/21/42/school-1555910__340.png");
+        break;
+    }
+  })
 });
